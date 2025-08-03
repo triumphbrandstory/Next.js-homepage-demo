e9b827d747cc9b0d69be81bbd8d92d43d444af2d
triumphbrandstory <triumphbrandstory@gmail.com>
2025-08-03 15:01:24 +0600

    fix issue

--- src/@components/Homepage/HeroSection/HeroCTASection.tsx
+++ src/@components/Homepage/HeroSection/HeroCTASection.tsx
@@ -10,15 +10,15 @@ interface HeroCTASectionProps {
 
 const HeroCTASection = ({ cta }: HeroCTASectionProps) => {
     return (
-        <div className="w-full space-y-3 max-w-md order-2 lg:order-1 lg:mb-8">
+        <div className="w-full space-y-3 max-w-[313px] order-2 lg:order-1 lg:mb-8">
             <Button
                 size="lg"
-                className="w-full max-w-md text-xl font-bold tracking-wide !py-2"
+                className="w-full max-w-[313px] text-xl font-bold tracking-wide !py-2"
                 icon={ChevronRight}
             >
                 {cta.text}
             </Button>
-            <p className="text-white text-sm font-medium text-center">
+            <p className="text-white text-sm font-medium text-center mt-5 md:mt-3">
                 {cta.subText}
             </p>
         </div>
--- src/@components/Homepage/HeroSection/HeroLegalText.tsx
+++ src/@components/Homepage/HeroSection/HeroLegalText.tsx
@@ -7,11 +7,11 @@ const HeroLegalText = ({ legalText, copyright }: HeroLegalTextProps) => {
     const currentYear = new Date().getFullYear().toString();
 
     return (
-        <div className="space-y-2 text-xs text-dark-gray order-1 lg:order-2 mb-8">
-            <p className="text-center lg:text-left leading-5">
+        <div className="space-y-2 text-dark-gray order-1 lg:order-2 mb-8">
+            <p className="text-xs text-center lg:text-left leading-5">
                 {legalText}
             </p>
-            <p className="text-center lg:text-left">
+            <p className="text-[10px] lg:text-xs text-center lg:text-left">
                 {copyright.replace('{year}', currentYear)}
             </p>
         </div>
